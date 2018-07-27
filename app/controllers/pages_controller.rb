250083722e9c77dad69603e9c155e8356e62db3f
nathalia coutinho <nathccoutinhos@gmail.com>
2018-07-27 13:08:50 +0200

    applied style and enhanced front-end design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def home
-    @mushrooms = Mushroom.all.limit(10)
+    @mushrooms = Mushroom.all.limit(3)
   end
 
   def filter
     # puts params[:filter].split("|")
 
 
-    @all_mushrooms = Mushroom.all.limit(20)
+    @all_mushrooms = Mushroom.all
     @filtered_edible =  Mushroom.where(edible: params[:filter])
     @filtered_color =  Mushroom.where(cap_color: params[:filter])
     respond_to do |f|
